dae04b1da242c4aa2d36102c2bd1c916285cb6c3
Carlos Linares <cadalisa@gmail.com>
2013-06-03 22:32:30 -0430

    Reponsive del menu

--- TrackTweets/static/js/main.js
+++ TrackTweets/static/js/main.js
@@ -5,20 +5,18 @@ function handlerDocumentReady(){
 
 	//desplegar menu de usuario
 	$('#user').hoverIntent(function(){
-		$('#menuUser').slideToggle('fast');
+		$('.menuUser').slideToggle('fast');
 	});
 
+	$('#userMenu').hoverIntent(function(){
+		$('.menuUser').slideToggle('fast');
+	});	
 
 	$('#IconMenu-display').on('click',handlerClickMenu);
 
 }
 function handlerClickMenu (e) {
-	css ={
-		display: 'inline-block',
-		heigth: 'auto'
-	}
 
 	$('#menu-display').slideToggle('fast');
-	// $('#menu-display').css(css);
 
 }
